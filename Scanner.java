a134a3aa4e5f5dc0365a52e1c08621b127005990
great754 <greatabhieyighan@gmail.com>
2025-10-12 19:39:08 -0400

    created the directory

---
+++ Scanner.java
@@ -0,0 +1,234 @@
+package filtR;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static filtR.TokenType.*;
+
+public class Scanner {
+  private final String source;
+  private final List<Token> tokens = new ArrayList<>();
+  private int start = 0;
+  private int current = 0;
+  private int line = 1;
+
+  private static final Map<String, TokenType> keywords;
+
+  // map of reserved words
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and", AND);
+    keywords.put("class", CLASS);
+    keywords.put("else", ELSE);
+    keywords.put("false", FALSE);
+    keywords.put("function", FUNCTION);
+    keywords.put("for", FOR);
+    keywords.put("if", IF);
+    keywords.put("or", OR);
+    keywords.put("print", PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("true", TRUE);
+    keywords.put("while", WHILE);
+    keywords.put("not", NOT);
+    keywords.put("null", NULL);
+    keywords.put("import", IMPORT);
+    keywords.put("export", EXPORT);
+    keywords.put("use", USE);
+    keywords.put("as", AS);
+    keywords.put("save", SAVE);
+    keywords.put("view", VIEW);
+    keywords.put("add", ADD);
+    keywords.put("remove", REMOVE);
+    keywords.put("set", SET);
+    keywords.put("where", WHERE);
+    keywords.put("filter", FILTER);
+    keywords.put("rename", RENAME);
+    keywords.put("to", TO);
+    keywords.put("missing", MISSING);
+    keywords.put("drop", DROP);
+    keywords.put("columns", COLUMNS);
+    keywords.put("column", COLUMN);
+    keywords.put("row", ROW);
+    keywords.put("rows", ROWS);
+    keywords.put("from", FROM);
+    keywords.put("with", WITH);
+    keywords.put("in", IN);
+    keywords.put("fill", FILL);
+    keywords.put("blanks", BLANKS);
+
+  }
+
+  Scanner(String source) {
+    this.source = source;
+  }
+
+  List<Token> scanTokens() {
+    while (!isAtEnd()) {
+      // We are at the beginning of the next lexeme.
+      start = current;
+      scanToken();
+    }
+
+    tokens.add(new Token(EOF, "", null, line));
+    return tokens;
+  }
+
+  private void scanToken() {
+    char c = advance();
+    switch (c) {
+      case '(': addToken(LEFT_PAREN); break;
+      case ')': addToken(RIGHT_PAREN); break;
+      case '{': addToken(LEFT_BRACE); break;
+      case '}': addToken(RIGHT_BRACE); break;
+      case ',': addToken(COMMA); break;
+      case '.': addToken(DOT); break;
+      case '-': addToken(MINUS); break;
+      case '+': addToken(PLUS); break;
+      case ';': addToken(SEMICOLON); break;
+      case '*': addToken(STAR); break; 
+
+    // comparison operators
+      case '!':
+        addToken(match('=') ? BANG_EQUAL : BANG);
+        break;
+      case '=':
+        addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+        break;
+      case '<':
+        addToken(match('=') ? LESS_EQUAL : LESS);
+        break;
+      case '>':
+        addToken(match('=') ? GREATER_EQUAL : GREATER);
+        break;
+
+    // handling slashes
+       case '/':
+        if (match('/')) {
+            while (peek() != '\n' && !isAtEnd())
+                advance();
+        } else {
+            addToken(SLASH);
+        }
+        break;
+    
+    // whitespace
+      case ' ':
+      case '\r':
+      case '\t':
+        break;
+
+      case '\n':
+        line++;
+        break;
+
+      case '"': string(); break;
+
+
+    // unexpected
+      default:
+        if (isDigit(c)) {
+            number();
+        } else if (isAlpha(c)) {
+            identifier();
+        } else {
+            filtR.error(line, "Unexpected character.");
+        }
+        break;
+    }
+  }
+
+  private void identifier() {
+    while (isAlphaNumeric(peek())) advance();
+
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if (type == null) type = IDENTIFIER;
+    addToken(type);
+  }
+
+  private void number() {
+    while (isDigit(peek())) advance();
+
+    // Look for a fractional part.
+    if (peek() == '.' && isDigit(peekNext())) {
+      // Consume the "."
+      advance();
+
+      while (isDigit(peek())) advance();
+    }
+
+    addToken(NUMBER,
+        Double.parseDouble(source.substring(start, current)));
+  }
+
+   private void string() {
+    while (peek() != '"' && !isAtEnd()) {
+      if (peek() == '\n') line++;
+      advance();
+    }
+
+    if (isAtEnd()) {
+      filtR.error(line, "Unterminated string.");
+      return;
+    }
+
+    // The closing ".
+    advance();
+
+    // Trim the surrounding quotes.
+    String value = source.substring(start + 1, current - 1);
+    addToken(STRING, value);
+  }
+
+  private boolean match(char expected) {
+    if (isAtEnd()) return false;
+    if (source.charAt(current) != expected) return false;
+
+    current++;
+    return true;
+  }
+
+  private char peek() {
+    if (isAtEnd()) return '\0';
+    return source.charAt(current);
+  }
+
+   private char peekNext() {
+    if (current + 1 >= source.length()) return '\0';
+    return source.charAt(current + 1);
+  } 
+
+   private boolean isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') ||
+           (c >= 'A' && c <= 'Z') ||
+            c == '_';
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
+  }
+
+   private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
+  } 
+
+   private boolean isAtEnd() {
+    return current >= source.length();
+  }
+
+  private char advance() {
+    return source.charAt(current++);
+  }
+
+  private void addToken(TokenType type) {
+    addToken(type, null);
+  }
+
+  private void addToken(TokenType type, Object literal) {
+    String text = source.substring(start, current);
+    tokens.add(new Token(type, text, literal, line));
+  }
+  
+}
---
+++ TokenType.java
@@ -0,0 +1,30 @@
+package filtR;
+
+enum TokenType {
+    // Single-character tokens.
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,       // ( ) { }
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,        // , . - + ; / *
+
+    // One or two character tokens.
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Literals.
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords.
+    AND, CLASS, ELSE, FALSE, FUNCTION,
+    FOR, IF, OR, PRINT,
+    RETURN, TRUE, WHILE, NOT,
+
+    // specific to our language
+    IMPORT, EXPORT, USE, AS, SAVE, VIEW,
+    ADD, REMOVE, SET, WHERE, FILTER, RENAME, TO,
+    MISSING, DROP, COLUMNS, COLUMN, ROW, ROWS, FROM, WITH, IN,
+    FILL, BLANKS, NULL,
+
+
+    EOF
+}
